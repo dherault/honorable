0eb0b2ae3255bd5eb6ed717fd632312197d9cb2e
David Hérault <dherault@gmail.com>
2022-04-13 14:00:31 +0300

    Normalize designer index.html

--- designer/src/index.js
+++ designer/src/index.js
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-createRoot(document.getElementById('app')).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
