6106e5736ffe2a16e87d25c703b6a8d9ce106efd
David Hérault <dherault@gmail.com>
2022-04-23 12:45:58 +0300

    About to refactor menu

--- storybook/src/stories/Select.stories.jsx
+++ storybook/src/stories/Select.stories.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { MenuItem, Select } from 'honorable'
+import { Menu, MenuItem, Select } from 'honorable'
 
 export default {
   title: 'Select',
@@ -10,22 +10,64 @@ export default {
   },
 }
 
-function Template(args) {
-  const [value, setValue] = useState(null)
+function Template({ items, initialValue, ...args }) {
+  const [value, setValue] = useState(initialValue)
+
+  function renderItem({ text, items }) {
+    return (
+      <MenuItem
+        key={text}
+        value={text}
+      >
+        {text}
+        {Array.isArray(items) && (
+          <Menu>
+            {items.map(renderItem)}
+          </Menu>
+        )}
+      </MenuItem>
+    )
+  }
 
   return (
     <Select
+      {...args}
       value={value}
       onChange={event => setValue(event.target.value)}
-      {...args}
     >
-      <MenuItem value={0}>JavaScript</MenuItem>
-      <MenuItem value={1}>HTML</MenuItem>
-      <MenuItem value={2}>CSS</MenuItem>
+      {items.map(renderItem)}
     </Select>
   )
 }
 
+const items = [
+  { text: 'For' },
+  { text: 'CSS' },
+  { text: 'Lovers' },
+  { text: 'And' },
+]
+
+function makeItems(items, depth = 1) {
+  if (depth <= 0) return items
+
+  return items.map((item, i) => ({
+    ...item,
+    items: i % 2 === depth % 2 ? null : makeItems(items, depth - 1),
+  }))
+}
+
 export const Default = Template.bind({})
 Default.args = {
+  items,
+}
+
+export const Selected = Template.bind({})
+Selected.args = {
+  items,
+  initialValue: 'Lovers',
+}
+
+export const SubMenu = Template.bind({})
+SubMenu.args = {
+  items: makeItems(items, 6),
 }
