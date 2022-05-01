4c0ce497b04b4c17d6be23bd122d1c10ebfe13ca
David Hérault <dherault@gmail.com>
2022-05-01 13:13:06 +0300

    Add comment

--- packages/honorable/src/utils/resolveDefaultProps.ts
+++ packages/honorable/src/utils/resolveDefaultProps.ts
@@ -6,6 +6,7 @@ import filterObject from './filterObject'
 
 function resolveDefaultProps(defaultProps: DefaultProps, props: object, theme: HonorableTheme): object {
   if (Array.isArray(defaultProps)) {
+    // We use merge here becasue some style props are deeply nested objects
     return defaultProps.reduce((acc, x) => merge(acc, resolveDefaultProps(x, props, theme)), {})
   }
 
