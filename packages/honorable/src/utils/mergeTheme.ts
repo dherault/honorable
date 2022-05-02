2ed64366119394624ec62dc86017a8c2d0efdb8c
David Hérault <dherault@gmail.com>
2022-05-02 15:15:55 +0300

    Publish and upgrade

--- packages/honorable/src/utils/mergeTheme.ts
+++ packages/honorable/src/utils/mergeTheme.ts
@@ -1,11 +1,17 @@
-import merge from 'lodash.merge'
+import mergeWith from 'lodash.mergewith'
 
 import { HonorableTheme } from '../types'
 
 import defaultTheme from '../data/defaultTheme'
 
+function customizer(target: any, source: any) {
+  if (Array.isArray(target)) {
+    return [...target, ...source]
+  }
+}
+
 function mergeTheme(...themes: HonorableTheme[]): HonorableTheme {
-  return merge({}, defaultTheme, ...themes)
+  return mergeWith({}, defaultTheme, ...themes, customizer)
 }
 
 export default mergeTheme
