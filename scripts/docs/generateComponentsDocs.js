6c0820a3b7112b0dcd8c243d9cd994f03e398f51
David Hérault <dherault@gmail.com>
2022-05-27 20:03:52 +0300

    Generate docs json

--- scripts/docs/generateComponentsDocs.js
+++ scripts/docs/generateComponentsDocs.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line
+const fs = require('fs')
+// eslint-disable-next-line
+const path = require('path')
+
+// eslint-disable-next-line
 const extractProps = require('./components/extractProps')
 // eslint-disable-next-line
 const extractDemo = require('./components/extractDemo')
@@ -7,11 +12,22 @@ function generateComponentDocs() {
   const componentToProps = extractProps()
   const componentToDemo = extractDemo()
 
-  generateComponentPage('Accordion', componentToProps.Accordion, componentToDemo.Accordion)
-}
+  ;['Accordion'].forEach(component => {
+    const docsDataLocation = path.join(__dirname, `../../site/src/data/components/${component}.json`)
 
-function generateComponentPage(name, props, demo) {
-
+    fs.writeFileSync(
+      docsDataLocation,
+      JSON.stringify(
+        {
+          props: componentToProps[component],
+          ...componentToDemo[component],
+        },
+        null,
+        2
+      ),
+      'utf8'
+    )
+  })
 }
 
 generateComponentDocs()
