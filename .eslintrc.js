fdefedd231a7d3e9f34343386d2769d05e7fb945
David Hérault <dherault@gmail.com>
2022-12-22 00:26:33 +0100

    Edit .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,5 +45,6 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 }
